8ceb790403757d7c18c7521f18c0a8156f05139c
Danilo Calhes <dancps96@gmail.com>
2019-03-24 14:41:48 -0300

    Adicionei os aruivos do primeiro teste
    Foi adicionada uma pasta para o arquivo do ESP32 e uma para o do Arduino.

---
+++ ARD_UARTreader/ARD_UARTreader.ino
@@ -0,0 +1,57 @@
+/*
+ *  Esse código le o que recebe do ESP.
+ *  
+ *  Pins:
+ *  0 -> HV4 do conv logic -> TX0 do ESP32 (recebe info)
+ *  A4 -> SCA I2C
+ *  A5 -> SCL I2C
+ *  
+ *  Para esse programa foi usado um arduino conectado com o ESP32
+ *  O ESP envia os dados presentes nesse código por meio da saida
+ *  TX0 e o Arduino recebe em sua porta RX. Após isso é printado 
+ *  na tela LCD I2C o wifi de maior intensidade.
+ *  
+ *  PS: é importante que TODAS as comunicações entre o ESP32 e o
+ *  Arduino sejam feitas com intermedio de um conversor de nível 
+ *  lógico, visto que o Arduino trabala em 5V e o ESP32 em 3.3V.
+ *  
+ *  
+ *  Talvez isso seja bom:
+ *  https://github.com/Robot-Will/Stino
+ */
+
+#include "Wire.h"
+#include "LiquidCrystal_I2C.h"
+int lcdColumns = 16;
+int lcdRows = 2;
+int incomingByte = 0; 
+
+// Inicializa o display no endereco 0x27
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  lcd.begin (16,2);// initialize LCD
+}
+
+void loop() {
+  // send data only when you receive data:
+    
+  //lcd.clear();
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    
+    Serial.println(Serial.readString());
+  }
+  
+  // put your main code here, to run repeatedly:
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("ESP32:");
+  lcd.setCursor(0,1);
+  lcd.print(Serial.readString());
+  delay(1000);
+}
---
+++ WiFiScanToSerial/WiFiScanToSerial.ino
@@ -0,0 +1,67 @@
+/*
+ *  This sketch demonstrates how to scan WiFi networks.
+ *  The API is almost the same as with the WiFi Shield library,
+ *  the most obvious difference being the different file you need to include:
+ *  
+ *  Pins:
+ *  Vin -> 5V do Arduino
+ *  GND -> GND do Arduino
+ *  TX0 -> LV4 do conv logico 
+ *  
+ *  Para esse programa foi usado um arduino conectado com o ESP32
+ *  O ESP envia os dados presentes nesse código por meio da saida
+ *  TX0 e o Arduino recebe em sua porta RX. Após isso é printado 
+ *  na tela LCD I2C o wifi de maior intensidade.
+ *  
+ *  PS: é importante que TODAS as comunicações entre o ESP32 e o
+ *  Arduino sejam feitas com intermedio de um conversor de nível 
+ *  lógico, visto que o Arduino trabala em 5V e o ESP32 em 3.3V.
+ *  
+ *  
+ *  Talvez isso seja bom:
+ *  https://github.com/Robot-Will/Stino
+ */
+#include "WiFi.h"
+#include <Wire.h>
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    
+    //Serial.println("Setup done");
+}
+
+void loop()
+{
+    //Serial.println("scan start");
+
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    //Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        //Serial.print(n);
+        //Serial.println(" networks found");
+        for (int i = 0; i < 1; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+        }
+    }
+    //Serial.println("");
+
+    // Wait a bit before scanning again
+    delay(500);
+}
